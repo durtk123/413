4b6b12179f078e5c2afed1a05da0032e389bf0a5
durtk123 <149846632+durtk123@users.noreply.github.com>
2023-11-18 10:47:16 +0900

    Update cgw.py

--- cgw.py
+++ cgw.py
@@ -9,7 +9,8 @@ with tab1:
    st.header("Titanic Dataset", divider='gray')
    st.image("https://static.streamlit.io/examples/titanic.jpeg", width=200)
    tab1, tab2, tab3 = st.tabs(["Sex", "Pclass", "Embarked"])
-
+   with tab1:
+      st.bar_chart(t, x="Sex", y="Survived" )
 with tab2:
    st.header("A dog")
    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
