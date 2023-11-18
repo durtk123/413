656b0385a79c23ca54c3b61a991c15cd18eee060
durtk123 <149846632+durtk123@users.noreply.github.com>
2023-11-18 09:51:14 +0900

    Update cgw.py

--- cgw.py
+++ cgw.py
@@ -6,7 +6,7 @@ st.title("차건우")
 tab1, tab2, tab3 = st.tabs(["Home", "About me", "Projects"])
 
 with tab1:
-   st.header("Titanic Dataset', divider='rainbow')
+   st.header("Titanic Dataset", divider='rainbow')
    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:
